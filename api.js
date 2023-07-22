f5779e7287d56181005e176e277a088fc60d9c7f
Mike Fulcher <michael.fulcher@gmail.com>
2023-07-22 15:37:11 +1200

    Initial API

---
+++ api.js
@@ -0,0 +1,277 @@
+import dgram from "dgram"
+import crypto from "crypto"
+
+const KEY = "d5d967ca-5d37-4c"
+const BRIDGE_ADDRESS = "192.168.178.22"
+const UNICAST_PORT = 32100
+const MULTICAST_PORT = 32101
+const MULTICAST_ADDRESS = "238.0.0.18"
+const SELF_ADDRESS = "0.0.0.0"
+const BRIDGE_TYPE = "02000001"
+const DEVICE_TYPE = "10000000"
+
+class Bridge {
+  constructor(api, mac, token) {
+    this.api = api
+    this.mac = mac
+    this.token = token
+    this.devices = new Map()
+
+    this.setAccessToken()
+    this.update()
+  }
+
+  update() {
+    this.readDevice()
+    this.getDevices()
+  }
+
+  setState(state) {
+    this.state = state
+  }
+
+  readDevice() {
+    this.send("ReadDevice").then(({ data }) => {
+      this.setState(data)
+    })
+  }
+
+  getDevices() {
+    this.send("GetDeviceList").then(({ fwVersion, ProtocolVersion, data }) => {
+      this.fwVersion = fwVersion
+      this.ProtocolVersion = ProtocolVersion
+
+      this.setDevices(data)
+    })
+  }
+
+  encrypt(token, key) {
+    const cipher = crypto.createCipheriv("aes-128-ecb", key, null)
+    const buffer = Buffer.concat([cipher.update(token), cipher.final()])
+
+    return buffer.toString("hex").substring(0, 32).toUpperCase()
+  }
+
+  // TODO: check if any previously known devices are no longer in the set
+  setDevices(deviceData) {
+    for (const { mac, deviceType } of deviceData) {
+      if (deviceType === DEVICE_TYPE) {
+        this.devices.set(mac, new Device(this, mac))
+      }
+    }
+  }
+
+  setAccessToken() {
+    this.accessToken = this.encrypt(this.token, KEY)
+  }
+
+  send(msgType, options = {}) {
+    return new Promise((resolve, _reject) => {
+      this.api.queueCall(msgType, {
+        mac: this.mac,
+        deviceType: BRIDGE_TYPE,
+        accessToken: this.accessToken,
+        ...options
+      }, data => resolve(data))
+    })
+  }
+}
+
+class Device {
+  constructor(bridge, mac) {
+    this.bridge = bridge
+    this.mac = mac
+
+    this.update()
+  }
+
+  update() {
+    this.readDevice()
+  }
+
+  setState(state) {
+    this.state = state
+  }
+
+  readDevice() {
+    return this.send("ReadDevice").then(({ data }) => {
+      this.setState(data)
+    })
+  }
+
+  writeDevice(data) {
+    return this.send("WriteDevice", { data }).then(({ data }) => {
+      this.setState(data)
+    })
+  }
+
+  send(msgType, options = {}) {
+    return this.bridge.send(msgType, {
+      mac: this.mac,
+      deviceType: DEVICE_TYPE,
+      ...options
+    })
+  }
+}
+
+export default class ApiClient {
+  constructor(logger) {
+    this.log = logger || console
+    this.bridges = new Map()
+    this.currentMessage = null
+    this.messageQueue = []
+
+    // Always listen for multicast messages so that Report and Heartbeat
+    // messages can be received at all times
+    this.startListening()
+
+    // Initial device discovery
+    this.discover()
+  }
+
+  startListening() {
+    this.log.debug(`Listening on multicast ${MULTICAST_ADDRESS}:${MULTICAST_PORT}`)
+
+    const multicast = dgram.createSocket("udp4")
+
+    multicast.bind(MULTICAST_PORT, SELF_ADDRESS, () => {
+      multicast.addMembership(MULTICAST_ADDRESS, SELF_ADDRESS)
+    })
+
+    // Multicast messages announce changes to the state of each blind
+    multicast.on("message", (data, _remote) => {
+      const parsedData = JSON.parse(data)
+      const { msgType, mac, token, deviceType, data: state } = parsedData
+
+      this.log.debug(`Multicast message ${msgType} from ${mac}: ${data}`)
+
+      if (msgType === "Heartbeat" && deviceType === BRIDGE_TYPE) {
+        this.handleHeartbeat(parsedData)
+      } else if (msgType === "Report") {
+        this.handleReport(parsedData)
+      }
+    })
+
+    multicast.on("close", () => {
+      this.log.debug(`Multicast socket closed! Reconnecting...`)
+      this.startListening()
+    })
+  }
+
+  handleHeartbeat({ mac, token, data: state }) {
+    // Known bridge: check that the correct number of devices are known
+    // Unknown bridge: add it and its devices
+    if (this.bridges.has(mac)) {
+      const bridge = this.bridges.get(mac)
+
+      if (bridge.devices.size !== state.numberOfDevices) {
+        this.log.debug("Known bridge has different number of devices")
+        bridge.update()
+      }
+    } else {
+      this.log.debug("New bridge discovered")
+      this.bridges.set(mac, new Bridge(this, mac, token))
+    }
+  }
+
+  // TODO: check that this contains all state, or just the changed state
+  handleReport(parsedData) {
+    const { mac } = parsedData
+
+    this.bridges.forEach((bridge, _) => {
+      if (bridge.devices.has(mac)) {
+        bridge.devices.get(mac).setState(parsedData)
+      }
+    })
+  }
+
+  discover() {
+    this.call("GetDeviceList", {}, ({ mac, token, deviceType }) => {
+      if (deviceType === BRIDGE_TYPE) {
+        this.bridges.set(mac, new Bridge(this, mac, token))
+      }
+    })
+  }
+
+  queueCall(msgType, options, callback) {
+    this.messageQueue.push([msgType, options, callback])
+    this.callNext()
+  }
+
+  call(msgType, { data, mac, deviceType, accessToken }, callback) {
+    const responseMsgType = `${msgType}Ack`
+    // When not sending to any particular device, keep the socket open for a set
+    // period of time in order to wait for all responses to come in
+    const keepAlive = mac == null
+    // NOTE: according to the spec an incrementing msgID value should be
+    // supplied, but in practice it doesn't appear to be necessary
+    const json = { msgType }
+    let timer
+
+    if (mac && deviceType && accessToken) {
+      json.mac = mac
+      json.deviceType = deviceType
+      json.AccessToken = accessToken
+
+      if (data) {
+        json.data = data
+      }
+    }
+
+    const msg = Buffer.from(JSON.stringify(json))
+    const unicast = dgram.createSocket("udp4")
+
+    function close() {
+      clearTimeout(timer)
+      unicast.close()
+    }
+
+    unicast.bind()
+
+    unicast.on("message", (data) => {
+      const parsedData = JSON.parse(data)
+      const { msgType, mac } = parsedData
+
+      // Unless expecting multiple responses, ignore messages from other devices
+      if (!keepAlive && json.mac !== mac) {
+        return close()
+      }
+
+      // Ignore messages without the expected response type
+      if (responseMsgType !== msgType) {
+        return close()
+      }
+
+      this.log.debug(`Unicast message ${msgType} from ${mac}: ${data}`)
+
+      callback(parsedData)
+
+      // Unless expecting multiple responses, now that a response has been
+      // received, the socket can be closed
+      if (!keepAlive) {
+        this.currentMessage = null
+
+        close()
+        this.callNext()
+      }
+    })
+
+    this.log.debug(`Sending ${msgType} to ${mac || "all"}`)
+
+    unicast.send(msg, 0, msg.length, UNICAST_PORT, BRIDGE_ADDRESS)
+
+    // Keep socket open a max of 5s
+    timer = setTimeout(() => close(), 5000)
+  }
+
+  callNext() {
+    // Bail if there's a call in progress or if there are no calls queued
+    if (this.currentMessage != null || this.messageQueue.length === 0) {
+      return
+    }
+
+    this.currentMessage = this.messageQueue.shift()
+
+    this.call(...this.currentMessage)
+  }
+}
