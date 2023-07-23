1625895e7701be916292bbe28334301a14d4c16f
Mike Fulcher <michael.fulcher@gmail.com>
2023-07-23 13:07:12 +1200

    Fix import

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { PLATFORM_NAME } from "./settings.js"
-import { MotionBlindsPlatform } from "./platform.js"
+import MotionBlindsPlatform from "./platform.js"
 
 export default (api) => {
   api.registerPlatform(PLATFORM_NAME, MotionBlindsPlatform)
