40ccc996e61ac54996d5dbbc34d83bac8bdb22a0
Mike Fulcher <michael.fulcher@gmail.com>
2023-08-07 10:36:12 +1200

    Missed this

--- settings.js
+++ settings.js
@@ -1,3 +1,5 @@
+import { version } from "./package.json" assert { type: "json" }
+
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
@@ -7,3 +9,5 @@ export const PLATFORM_NAME = "MotionBlinds"
  * This must match the name of your plugin as defined the package.json
  */
 export const PLUGIN_NAME = "homebridge-motion-blinds"
+
+export const PLUGIN_VERSION = version
